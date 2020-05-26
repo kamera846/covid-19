1e4a5f401ed33a958fff06f9ade3b6199fad4921
Rafliram <keerouk.ink@gmail.com>
2020-05-26 13:13:33 +0700

    (+) Dummy chart

--- app/src/main/java/com/example/covid19/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/example/covid19/ui/statistics/StatisticsFragment.kt
@@ -9,10 +9,15 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.covid19.R
+import com.github.mikephil.charting.charts.LineChart
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 
 class StatisticsFragment : Fragment() {
 
     private lateinit var statisticsViewModel: StatisticsViewModel
+    private lateinit var lineChart: LineChart
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -22,10 +27,39 @@ class StatisticsFragment : Fragment() {
         statisticsViewModel =
             ViewModelProviders.of(this).get(StatisticsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_statistics, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        statisticsViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
+        lineChart = root.findViewById(R.id.lineChart)
+        statisticsViewModel.data.observe(viewLifecycleOwner, Observer {
+            setData(it)
         })
+
         return root
     }
+
+    private fun setData(data: LineDataSet) {
+        //Part5
+        lineChart.xAxis.labelRotationAngle = 0f
+
+        //Part6
+        lineChart.data = LineData(data)
+
+        //Part7
+        lineChart.axisRight.isEnabled = false
+//            lineChart.xAxis.axisMaximum = j+0.1f
+
+        //Part8
+        lineChart.setTouchEnabled(true)
+        lineChart.setPinchZoom(true)
+
+        //Part9
+        lineChart.description.text = "Days"
+        lineChart.setNoDataText("No forex yet!")
+
+//            //Part10
+//            lineChart.animateX(1800, Easing.EaseInExpo)
+//
+//            //Part11
+//            val markerView = CustomMarker(this@ShowForexActivity, R.layout.marker_view)
+//            lineChart.marker = markerView
+    }
+
 }
--- app/src/main/java/com/example/covid19/ui/statistics/StatisticsViewModel.kt
+++ app/src/main/java/com/example/covid19/ui/statistics/StatisticsViewModel.kt
@@ -3,11 +3,34 @@ package com.example.covid19.ui.statistics
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.covid19.R
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineDataSet
 
 class StatisticsViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is Statistics Fragment"
+    private val _data = MutableLiveData<LineDataSet>().apply {
+        //Part1
+        val entries = ArrayList<Entry>()
+
+        //Part2
+        entries.add(Entry(1f, 10f))
+        entries.add(Entry(2f, 2f))
+        entries.add(Entry(3f, 7f))
+        entries.add(Entry(4f, 20f))
+        entries.add(Entry(5f, 16f))
+
+        //Part3
+        val vl = LineDataSet(entries, "My Type")
+
+        //Part4
+        vl.setDrawValues(false)
+        vl.setDrawFilled(true)
+        vl.lineWidth = 3f
+        vl.fillColor = R.color.colorAccent
+        vl.fillAlpha = R.color.colorRed
+
+        value = vl
     }
-    val text: LiveData<String> = _text
+    val data: LiveData<LineDataSet> = _data
 }
