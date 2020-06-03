c396062fa7535dff6d7003e7162958e46d7f5a10
Rafliram <keerouk.ink@gmail.com>
2020-06-03 20:26:52 +0700

    (+) Detail country

---
+++ app/src/main/java/com/example/covid19/ui/CountryDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.example.covid19.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.covid19.R
+
+class CountryDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_country_detail)
+    }
+}
